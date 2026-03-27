fdbe29f1cbf4a8ffc78084e591f6c2e232d82802
amarbardiansyah15-a11y <amar.bardiansyah15@gmail.com>
2026-03-28 00:29:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Scroll Reveal Animation
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Initial call to reveal elements on load
+reveal();
+
+// Simple Mobile Menu (Placeholder)
+const menuToggle = document.querySelector('.menu-toggle');
+if (menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        alert('Menu navigasi dapat dikembangkan lebih lanjut menjadi sidebar mobile.');
+    });
+}
